efcdfabe03c86033e2690969b8563e067c84eeb9
Agnieszka Ciepielewska <aciepielewska@mozilla.com>
2018-07-25 11:49:52 +0100

    don't have to initialize pipelien each time

--- canary/generator/transformators/transformers_exponential.py
+++ canary/generator/transformators/transformers_exponential.py
@@ -18,13 +18,15 @@ class AddMoveTransformer(TransformerExponential):
     def __init__(self, shift=None, change_date=None):
         """anomaly"""
         super(AddMoveTransformer, self).__init__()
+        self.is_shift = False if shift is None else True
+        self.is_date = False if change_date is None else True
         self.shift = shift
         self.change_date = change_date
 
     def transform(self, point_dict, y):
-        if self.change_date is None:
+        if not self.is_date:
             self.change_date = np.random.choice(list(point_dict['data'].keys()))
-        if self.shift is None:
+        if not self.is_shift:
             self.shift = np.random.uniform(-0.8, 0.8)
 
         y_copy = deepcopy(y)
@@ -41,6 +43,10 @@ class AddToSomeBucketSmoothTransformer(TransformerExponential):
     def __init__(self, percentage=None, mean=None, std=None, change_date=None):
         """anomaly"""
         super(AddToSomeBucketSmoothTransformer, self).__init__()
+        self.is_mean = False if mean is None else True
+        self.is_std = False if std is None else True
+        self.is_percentage = False if percentage is None else True
+        self.is_date = False if change_date is None else True
         self.percentage = percentage
         self.mean = mean
         self.std = std
@@ -48,14 +54,14 @@ class AddToSomeBucketSmoothTransformer(TransformerExponential):
 
     def transform(self, point_dict, y):
         y_copy = deepcopy(y)
-        if self.change_date is None:
+        if not self.is_date:
             self.change_date = np.random.choice(list(point_dict['data'].keys()))
         points = point_dict['data'][self.change_date]
-        if self.mean is None:
+        if not self.is_mean:
             self.mean = np.random.uniform(low=0, high=1) * max(point_dict['buckets'])
-        if self.std is None:
+        if not self.is_std:
             self.std = np.random.uniform(low=0, high=1) * self.mean
-        if self.percentage is None:
+        if not self.is_percentage:
             self.percentage = np.random.uniform(0.1, 0.4)
 
         shape = (self.mean / self.std) ** 2
@@ -73,17 +79,20 @@ class AddAnotherDistTransformer(TransformerExponential):
     def __init__(self, mean=None, std=None, change_date=None):
         """anomaly"""
         super(AddAnotherDistTransformer, self).__init__()
+        self.is_mean = False if mean is None else True
+        self.is_std = False if std is None else True
+        self.is_date = False if change_date is None else True
         self.mean = mean
         self.std = std
         self.change_date = change_date
 
     def transform(self, point_dict, y):
         y_copy = deepcopy(y)
-        if self.change_date is None:
+        if not self.is_date:
             self.change_date = np.random.choice(list(point_dict['data'].keys()))
-        if self.mean is None:
+        if not self.is_mean:
             self.mean = np.random.uniform(low=0, high=1) * max(point_dict['buckets'])
-        if self.std is None:
+        if not self.is_std:
             self.std = np.random.uniform(low=0.3, high=1) * self.mean
         shape = (self.mean / self.std) ** 2
         scale = self.mean / shape
@@ -100,10 +109,11 @@ class AddExpNoiseTransformer(TransformerExponential):
     def __init__(self, percentage=None):
         """not an anomaly"""
         super(AddExpNoiseTransformer, self).__init__()
+        self.is_percentage = False if percentage is None else True
         self.percentage = percentage
 
     def transform(self, point_dict, y):
-        if self.percentage is None:
+        if not self.is_percentage:
             self.percentage = np.random.uniform(0, 0.2)
         for date, points in point_dict['data'].items():
             points = [np.random.exponential(abs(p)) if
@@ -118,10 +128,11 @@ class AddTrendTransformer(TransformerExponential):
     def __init__(self, alpha=None):
         """not an anomaly, works on whole data"""
         super(AddTrendTransformer, self).__init__()
+        self.is_alpha = False if alpha is None else True
         self.alpha = alpha
 
     def transform(self, point_dict, y):
-        if self.alpha is None:
+        if not self.is_alpha:
             self.alpha = np.random.uniform(-0.001, 0.001)
         for i, (date, points) in enumerate(sorted(point_dict['data'].items())):
             trend = i * self.alpha
@@ -136,10 +147,11 @@ class AddWeekSeasonalityTransformer(TransformerExponential):
     def __init__(self, alpha=None):
         """not an anomaly, works on whole data"""
         super(AddWeekSeasonalityTransformer, self).__init__()
+        self.is_alpha = False if alpha is None else True
         self.alpha = alpha
 
     def transform(self, point_dict, y):
-        if self.alpha is None:
+        if not self.is_alpha:
             self.alpha = np.random.uniform(0, 0.1)
         week = []
         for i in range(7):
--- canary/generator/transformators/transformers_linear.py
+++ canary/generator/transformators/transformers_linear.py
@@ -10,21 +10,20 @@ class TransformerLinear(Transformer, ABC):
     def __init__(self):
         super(TransformerLinear, self).__init__(['linear'])
 
-    # def transform(self, point_dict, y):
-    #     raise NotImplementedError
-
 
 class AddMoveTransformer(TransformerLinear):
     def __init__(self, shift=None, change_date=None):
         """anomaly"""
         super(AddMoveTransformer, self).__init__()
+        self.is_shift = False if shift is None else True
+        self.is_date = False if change_date is None else True
         self.shift = shift
         self.change_date = change_date
 
     def transform(self, point_dict, y):
-        if self.change_date is None:
+        if not self.is_date:
             self.change_date = np.random.choice(list(point_dict['data'].keys()))
-        if self.shift is None:
+        if not self.is_shift:
             self.shift = np.random.uniform(-0.8, 0.8) * max(point_dict['buckets'])
 
         y_copy = deepcopy(y)
@@ -41,6 +40,10 @@ class AddToSomeBucketSmoothTransformer(TransformerLinear):
     def __init__(self, percentage=None, mean=None, std=None, change_date=None):
         """anomaly"""
         super(AddToSomeBucketSmoothTransformer, self).__init__()
+        self.is_mean = False if mean is None else True
+        self.is_std = False if std is None else True
+        self.is_percentage = False if percentage is None else True
+        self.is_date = False if change_date is None else True
         self.percentage = percentage
         self.mean = mean
         self.std = std
@@ -48,14 +51,14 @@ class AddToSomeBucketSmoothTransformer(TransformerLinear):
 
     def transform(self, point_dict, y):
         y_copy = deepcopy(y)
-        if self.change_date is None:
+        if not self.is_date:
             self.change_date = np.random.choice(list(point_dict['data'].keys()))
         points = point_dict['data'][self.change_date]
-        if self.mean is None:
+        if not self.is_mean:
             self.mean = np.random.uniform(low=0, high=1) * max(point_dict['buckets'])
-        if self.std is None:
+        if not self.is_std:
             self.std = np.random.uniform(low=0, high=1) * self.mean
-        if self.percentage is None:
+        if not self.is_percentage:
             self.percentage = np.random.uniform(0.1, 0.4)
 
         shape = (self.mean / self.std) ** 2
@@ -73,17 +76,20 @@ class AddAnotherDistTransformer(TransformerLinear):
     def __init__(self, mean=None, std=None, change_date=None):
         """anomaly"""
         super(AddAnotherDistTransformer, self).__init__()
+        self.is_mean = False if mean is None else True
+        self.is_std = False if std is None else True
+        self.is_date = False if change_date is None else True
         self.mean = mean
         self.std = std
         self.change_date = change_date
 
     def transform(self, point_dict, y):
         y_copy = deepcopy(y)
-        if self.change_date is None:
+        if not self.is_date:
             self.change_date = np.random.choice(list(point_dict['data'].keys()))
-        if self.mean is None:
+        if not self.is_mean:
             self.mean = np.random.uniform(low=0, high=1) * max(point_dict['buckets'])
-        if self.std is None:
+        if not self.is_std:
             self.std = np.random.uniform(low=0.3, high=1) * self.mean
         shape = (self.mean / self.std) ** 2
         scale = self.mean / shape
@@ -100,10 +106,11 @@ class AddGaussianNoiseTransformer(TransformerLinear):
     def __init__(self, std=None):
         """not an anomaly"""
         super(AddGaussianNoiseTransformer, self).__init__()
+        self.is_std = False if std is None else True
         self.std = std
 
     def transform(self, point_dict, y):
-        if self.std is None:
+        if not self.is_std:
             self.std = np.random.uniform(0, 0.2) * max(point_dict['buckets'])
         for date, points in point_dict['data'].items():
             noise = np.random.normal(0, self.std, len(points))
@@ -118,10 +125,11 @@ class AddTrendTransformer(TransformerLinear):
     def __init__(self, alpha=None):
         """not an anomaly, works on whole data"""
         super(AddTrendTransformer, self).__init__()
+        self.is_alpha = False if alpha is None else True
         self.alpha = alpha
 
     def transform(self, point_dict, y):
-        if self.alpha is None:
+        if not self.is_alpha:
             self.alpha = np.random.uniform(-0.001, 0.001) * max(point_dict['buckets'])
         for i, (date, points) in enumerate(sorted(point_dict['data'].items())):
             trend = i * self.alpha
@@ -136,10 +144,11 @@ class AddWeekSeasonalityTransformer(TransformerLinear):
     def __init__(self, alpha=None):
         """not an anomaly, works on whole data"""
         super(AddWeekSeasonalityTransformer, self).__init__()
+        self.is_alpha = False if alpha is None else True
         self.alpha = alpha
 
     def transform(self, point_dict, y):
-        if self.alpha is None:
+        if not self.is_alpha:
             self.alpha = np.random.uniform(0, 0.1)
         week = []
         for i in range(7):
